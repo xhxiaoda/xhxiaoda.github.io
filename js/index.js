319fba685e810b8cacfb509ed2a392370d5a1a7f
jschenda <js13661744896@163.com>
2019-12-10 14:44:47 +0800

    ..

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function () {
+    function focusImg(classname){
+        var swiper = new Swiper(classname, {
+            loop:true,
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+    focusImg(".swiper-container0");
+})
