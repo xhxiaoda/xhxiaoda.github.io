30bc66a2b6f6ccd1aed82ace995aeeb03add14f4
xhxiaoda <37108405+xhxiaoda@users.noreply.github.com>
2021-10-11 12:48:26 +0800

    Update jquery.photoClip.js

--- js/jquery.photoClip.js
+++ js/jquery.photoClip.js
@@ -223,7 +223,7 @@
 				myScroll.zoom(sf);
 		});
 
-		$('#imgHtml').html("6666");
+		$('#imgHtml').html("7777");
 		$('#touchBtn').bind('touchstart',function(e){
 			if (e.originalEvent.targetTouches.length > 1) {
 				// 当两根手指放上去的时候，将距离(distance)初始化。
